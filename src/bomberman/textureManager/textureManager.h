52c3686a75ec95b28cea3c0d8d2ffe2f07be6cc7
aksels.ledins <aksels.ledins@gmail.com>
2016-04-18 15:38:04 +0900

    basic texture manager

---
+++ src/bomberman/textureManager/textureManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+class TextureManager
+{
+private:
+
+  std::map<std::string, sf::Texture> textures;
+
+public:
+
+  /* load texture from a file */
+  void loadTexture(const std::string &name, const std::string &path);
+
+  /* get texture */
+  sf::Texture & getRef(const std::string &name);
+
+  TextureManager();
+  virtual ~TextureManager();
+};
