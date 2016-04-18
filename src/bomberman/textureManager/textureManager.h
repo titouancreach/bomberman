ebf96a47c52c0d4f37c31e4ef0aefa07afb56d7a
aksels.ledins <aksels.ledins@gmail.com>
2016-04-18 16:43:49 +0900

    indentation

--- src/bomberman/textureManager/textureManager.h
+++ src/bomberman/textureManager/textureManager.h
@@ -1,26 +1,27 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
 #include <map>
+
+#include <SFML/Graphics.hpp>
 
 namespace bomber {
 
-class TextureManager
-{
-private:
+  class TextureManager
+  {
+  private:
 
-  std::map<std::string, sf::Texture> textures;
+    std::map<std::string, sf::Texture> textures;
 
-public:
+  public:
 
-  /* load texture from a file */
-  void loadTexture(const std::string &name, const std::string &path);
+    /* load texture from a file */
+    void loadTexture(const std::string &name, const std::string &path);
 
-  /* get texture */
-  sf::Texture & getRef(const std::string &name);
+    /* get texture */
+    sf::Texture & getRef(const std::string &name);
 
-  TextureManager();
-  virtual ~TextureManager();
-};
+    TextureManager();
+    virtual ~TextureManager();
+  };
 
 }
