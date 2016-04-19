ded504de7b50991281c0a187276b145150f1d4cd
aksels.ledins <aksels.ledins@gmail.com>
2016-04-19 17:27:40 +0900

    map definition

--- src/bomberman/map/map.cc
+++ src/bomberman/map/map.cc
@@ -3,6 +3,15 @@
 namespace bomber {
 
 Map::Map() {
+  tile_size_ = 8;
+  width_ = 0;
+  height_ = 0;
+  num_regions_[0] = 1;
+}
+
+Map::Map(const std::string& filename, unsigned int width, unsigned int height) {
+  this->tile_size_ = 8;
+  load(filename, width, height);
 }
 
 Map::~Map() {
--- src/bomberman/map/map.h
+++ src/bomberman/map/map.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include "tile.h"
 
 namespace bomber {
@@ -8,10 +10,30 @@ class Map {
 
  public:
   Map();
+  Map(const std::string& filename, unsigned int width, unsigned int height);
+  
   virtual ~Map();
+
+  /* load from disk */
+  void load(const std::string& filename, unsigned int width,
+    unsigned int height, std::map<std::string, Tile& tiles);
+
+  /* save to disk */
+  void save(const std::string& filename);
+
+  /* draw to window */
+  void draw(sf::RenderWindow& window, float dt);
 
  private:
   Tile **data_;
+  unsigned int height_;
+  unsigned int width_;
+
+  /* tiles related */
+  std::vector<Tile> tiles_;
+  unsigned int tile_size_;
+  unsigned int num_selected_; // TODO REMOVE
+  unsigned int num_regions_;
 
 };
 
